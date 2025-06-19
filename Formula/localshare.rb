a74800aa2e21090945c9efdcc7a0c6373ca48f3b
val polyakov <val@polyakov.me>
2025-06-19 16:06:21 -0500

    Update to v1.4.1 - Auto-login feature

--- Formula/localshare.rb
+++ Formula/localshare.rb
@@ -1,22 +1,22 @@
 class Localshare < Formula
   desc "Share local directories over the internet instantly"
   homepage "https://github.com/localshare-io/localshare"
-  version "1.4.0"
+  version "1.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/localshare-io/localshare-releases/releases/download/v1.4.0/localshare-darwin-amd64"
-      sha256 "2f7021eed96412c78d2323be5331537491f58ad3ed04cc48db816976a96a6aa0"
+      url "https://github.com/localshare-io/localshare-releases/releases/download/v1.4.1/localshare-darwin-amd64"
+      sha256 "4cd816ddb097f1ad9b61cf70699520e5fd6f4980feddc48c5d390645f9dd9125"
     else
-      url "https://github.com/localshare-io/localshare-releases/releases/download/v1.4.0/localshare-darwin-arm64"
-      sha256 "952a6ae3ff0a0f43ad3d38571be3891c436416a875b24e71229e00e354fb1d75"
+      url "https://github.com/localshare-io/localshare-releases/releases/download/v1.4.1/localshare-darwin-arm64"
+      sha256 "d1ea74bf508ec04e9ce08763c0a7e69b1f14eb42e646f7a57e188d1a4250d807"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/localshare-io/localshare-releases/releases/download/v1.4.0/localshare-linux-amd64"
+      url "https://github.com/localshare-io/localshare-releases/releases/download/v1.4.1/localshare-linux-amd64"
       sha256 "aedcec2323d92c73032a80da3ac75d7dd36d887048344d77f43bd7370338af0e"
     end
   end
